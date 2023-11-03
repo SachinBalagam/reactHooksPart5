bf607c7ebdc8e4256e11e2c0f4dea5b1a6997e1d
SachinBalagam <balagamsachin337@gmail.com>
2023-11-03 03:52:16 +0000

    Final Commit

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -1,9 +1,97 @@
-import {LeaderboardContainer} from './styledComponents'
+import {useEffect, useState} from 'react'
+
+import Loader from 'react-loader-spinner'
+import LeaderboardTable from '../LeaderboardTable/index'
+import {
+  LeaderboardContainer,
+  LoadingViewContainer,
+  ErrorMessage,
+} from './styledComponents'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  inProgress: 'IN_PROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
 
 const Leaderboard = () => {
-  // Your code goes here...
+  const [apiResponse, setApiResponse] = useState({
+    status: apiStatusConstants.initial,
+    data: null,
+    errorMsg: null,
+  })
+
+  useEffect(() => {
+    setApiResponse({
+      status: apiStatusConstants.inProgress,
+      data: null,
+      errorMsg: null,
+    })
+    const getLeaderBoardData = async () => {
+      const url = 'https://apis.ccbp.in/leaderboard'
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJhaHVsIiwicm9sZSI6IlBSSU1FX1VTRVIiLCJpYXQiOjE2MjMwNjU1MzJ9.D13s5wN3Oh59aa_qtXMo3Ec4wojOx0EZh8Xr5C5sRkU',
+        },
+      }
+      const response = await fetch(url, options)
+      const responseData = await response.json()
+      if (response.ok) {
+        setApiResponse(prevApiResponse => ({
+          ...prevApiResponse,
+          status: apiStatusConstants.success,
+          data: responseData,
+        }))
+      } else {
+        setApiResponse(prevApiResponse => ({
+          ...prevApiResponse,
+          status: apiStatusConstants.failure,
+          errorMsg: responseData.error_msg,
+        }))
+      }
+    }
+    getLeaderBoardData()
+  }, [])
+
+  const renderLoadingView = () => (
+    <LoadingViewContainer>
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </LoadingViewContainer>
+  )
+  const renderSuccessView = () => {
+    const {data} = apiResponse
+    console.log(data)
+    const formattedData = data.leaderboard_data.map(each => ({
+      id: each.id,
+      language: each.language,
+      name: each.name,
+      rank: each.rank,
+      score: each.score,
+      profileImgUrl: each.profile_image_url,
+      timeSpent: each.time_spent,
+    }))
+    return <LeaderboardTable leaderboardData={formattedData} />
+  }
+  const renderFailureView = () => {
+    const {errorMsg} = apiResponse
+    return <ErrorMessage>{errorMsg}</ErrorMessage>
+  }
+
   const renderLeaderboard = () => {
-    // Your code goes here...
+    const {status} = apiResponse
+    switch (status) {
+      case apiStatusConstants.inProgress:
+        return renderLoadingView()
+      case apiStatusConstants.success:
+        return renderSuccessView()
+      case apiStatusConstants.failure:
+        return renderFailureView()
+      default:
+        return null
+    }
   }
 
   return <LeaderboardContainer>{renderLeaderboard()}</LeaderboardContainer>
